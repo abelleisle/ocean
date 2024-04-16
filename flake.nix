0154ae2924b07dbcb6e34cf21bea778e33b9c8e9
abelleisle <abelleisle@protonmail.com>
2024-04-15 23:05:25 -0500

    Updated zls

--- flake.nix
+++ flake.nix
@@ -4,16 +4,26 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    zig-overlay.url = "github:mitchellh/zig-overlay";
-    zig-overlay.inputs.nixpkgs.follows = "nixpkgs";
+    zig-overlay = {
+      url = "github:mitchellh/zig-overlay/5cf2374c87cbe48139d1571360dcd7dd4807ef1c";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zls-overlay = {
+      url = "github:zigtools/zls";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        # zig-overlay.follows = "zig-overlay";
+      };
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, zig-overlay, ... }:
+  outputs = { self, nixpkgs, flake-utils, zig-overlay, zls-overlay, ... }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
       };
-      zig = zig-overlay.packages.${system}.master-2024-01-07;
+      zig = zig-overlay.packages.${system}.master;
+      zls = zls-overlay.packages.${system}.default;
     in {
       devShells.default = pkgs.mkShell {
         nativeBuildInputs = with pkgs; [
